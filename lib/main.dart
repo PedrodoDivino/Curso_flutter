037f40ce565fc18381c11f06b38ffb0b36c0707c
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-07-25 10:50:20 -0300

    [Pedro] Primeiro commit - Curso de Flutter #6 - Estados (StatelessWidget)

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// ignore: unnecessary_import
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  // ignore: avoid_unnecessary_containers
+  runApp(Container(
+    child: const Center(
+      child: Text('Pedro Henrique',
+     textDirection: TextDirection.ltr,
+    style: TextStyle(color: Colors.white,
+     fontSize:50.0 ),
+    )
+    )
+  ));
+}
+// widget são como componentes
+
