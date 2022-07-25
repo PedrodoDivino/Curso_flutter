f9ee345bd471aeec780c7d00ac9f717729ddaf22
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-07-25 11:07:42 -0300

    [Pedro] Curso de Flutter #6 - Estados (StatelessWidget)

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   // ignore: avoid_unnecessary_containers
-  runApp(Container(
-    child: const Center(
-      child: Text('Pedro Henrique',
-     textDirection: TextDirection.ltr,
-    style: TextStyle(color: Colors.white,
-     fontSize:50.0 ),
-    )
-    )
-  ));
+  runApp(const AppWidget(title: 'Funcionou!',));
 }
 // widget são como componentes
 
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({super.key, required this.title,});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
+    return Container(
+      child:  Center(
+        child: Text(
+         title,
+          textDirection: TextDirection.ltr,
+          style: const TextStyle(color: Color.fromARGB(255, 92, 76, 163), fontSize: 50.0),
+        ),
+      ),
+    );
+  }
+}
