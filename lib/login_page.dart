20310ec66dae54519d8af8301581a131352f39c9
pedrododivino <pedrohenriquebatera@hotmail.com>
2022-07-26 12:20:42 -0300

    [Pedro] Curso de Flutter #23 - Stack

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -15,63 +15,79 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String senha = '';
 
+  Widget _body() {
+    return (SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 250,
+                height: 250,
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              Container(height: 30),
+              TextField(
+                onChanged: (text) {
+                  email = text;
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  label: Text('Email'),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                onChanged: (text) {
+                  senha = text;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                  label: Text('Senha'),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 15),
+              RaisedButton(
+                onPressed: () {
+                  if (email == 'pedro@email.com' && senha == '123') {
+                    Navigator.of(context).pushReplacementNamed('/Home');
+                    MaterialPageRoute(builder: ((context) => HomePage()));
+                  } else {
+                    print('Senha ou Email, invalido!');
+                  }
+                },
+                child: Text('Entrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: (SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
+        body: Stack(
+      children: [
+        SizedBox(
           height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                    width: 250,
-                    height: 250,
-                    child: Image.asset('assets/images/logo.png'),
-                    ),
-                Container(height: 30),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    label: Text('Email'),
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    senha = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    label: Text('Senha'),
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 15),
-                RaisedButton(
-                  onPressed: () {
-                    if (email == 'pedro@email.com' && senha == '123') {
-                      Navigator.of(context).pushReplacementNamed('/Home');
-                      MaterialPageRoute(builder: ((context) => HomePage()));
-                    } else {
-                      print('Senha ou Email, invalido!');
-                    }
-                  },
-                  child: Text('Entrar'),
-                ),
-              ],
-            ),
+          child: Image.asset(
+            'assets/images/fundo.jpg',
+            fit: BoxFit.cover,
           ),
         ),
-      )),
-    );
+        Container(
+          color: Colors.black.withOpacity(0.3),
+        ),
+        _body(),
+      ],
+    ));
   }
 }
